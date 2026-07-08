d9423f3138125b6945ec2b775bd3cf29f284b352
Anupama <yourgithubemail@example.com>
2026-07-08 09:53:22 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+
+# Load API key
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+
+    blog = ""
+
+    if request.method == "POST":
+
+        topic = request.form["topic"]
+
+        prompt = f"""
+        Write a professional blog on {topic}.
+
+        Include:
+        - Catchy title
+        - Introduction
+        - Headings
+        - Conclusion
+
+        Around 500 words.
+        """
+
+        response = model.generate_content(prompt)
+
+        blog = response.text
+
+    return render_template("index.html", blog=blog)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
